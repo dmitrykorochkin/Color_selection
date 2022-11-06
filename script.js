5a91e4d386cc20907b3c63a97b1b27ec495e3e5a
Dmitry Korochkin <abramslost@gmail.com>
2022-11-06 16:02:29 +0300

    script code

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const cols = document.querySelectorAll(".col");
 
 document.addEventListener('keydown', event => {
+    event.preventDefault();
     if(event.code.toLowerCase() === 'space') {
         setRandomColors()
     }
 })
+
+document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type;
+
+    if(type === 'lock') {
+        const node = event.target.tagName.toLowerCase() === 'i'
+            ? event.target
+            : event.target.children[0];
+
+        node.classList.toggle('fa-lock-open');
+        node.classList.toggle('fa-lock');
+    } else if (type === 'copy') {
+        copyToClickBoard(event.target.textContent);
+    }
+})
+
+function copyToClickBoard(text) {
+    return navigator.clipboard.writeText(text);
+}
 
 function genereteRandomColor() {
   const hexCodes = "0123456789ABCDEF";
@@ -16,21 +36,41 @@ function genereteRandomColor() {
 }
 
 function setRandomColors() {
+    const colors = [];
+
   cols.forEach(col => {
+    const isLocked = col.querySelector('i').classList.contains('fa-lock');
     const text = col.querySelector('h2');
     const button = col.querySelector('button');
-    const color = genereteRandomColor()
+    const color = genereteRandomColor();
+
+    if (isLocked) {
+        colors.push(text.textContent);
+        return;
+    }
+    colors.push(color)
 
     text.textContent = color;
     col.style.background = color;
     setTextColor(text, color);
     setTextColor(button, color);
   });
+
+  updateColorsHash(colors);
 }
 function setTextColor(text,color) {
     const luminance = chroma(color).luminance();
     text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
 
+function updateColorsHash(colors = []) {
+    document.location.hash = colors.map(col => {
+        return col.toString().substring(1);
+    }).join('-')
+}
+
+function getColorsFromHash() {
+    
+}
 
 setRandomColors();
